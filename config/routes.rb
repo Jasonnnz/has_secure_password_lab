d338d94446a0ab685626f4ac8796fbbc68c4bffd
Jason Zhou <jzhou35@buffalo.edu>
2020-12-23 11:57:02 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new', as: 'new_session'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:show, :new, :create]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
